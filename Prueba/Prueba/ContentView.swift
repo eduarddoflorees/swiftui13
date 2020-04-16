e50f684bfeb5148d7bfc2231c84326580ef3454c
eduarddoflorees <eduardo.flores@cetys.edu.mx>
2020-04-16 01:55:02 -0700

    Proyecto de Prueba

---
+++ Prueba/Prueba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Prueba
+//
+//  Created by DOSE Automaton on 16/04/20.
+//  Copyright © 2020 Eduardo F. A. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
